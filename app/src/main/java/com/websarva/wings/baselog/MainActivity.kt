ae2aeb1573db6edf0e6e281f11ffccc4e311c336
ryohamag <Ryojune21@gmail.com>
2024-03-08 22:51:41 +0900

    TopBar,BottomBar,FABの追加

--- app/src/main/java/com/websarva/wings/baselog/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/baselog/MainActivity.kt
@@ -1,15 +1,39 @@
 package com.websarva.wings.baselog
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.websarva.wings.baselog.ui.theme.BaseLogTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +46,52 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BaseLogTheme {
-        Greeting("Android")
+fun MainContent() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "新規試合データ作成")
+            }
+        },
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(text = "BaseLog")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        Icon(imageVector = Icons.Default.Home, contentDescription = "ホームボタン", modifier = Modifier.size(48.dp))
+                    }
+                    IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_storage_24), contentDescription = "打撃成績ボタン", modifier = Modifier.size(48.dp))
+                    }
+                    IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        Icon(painter = painterResource(id = R.drawable.baseline_show_chart_24), contentDescription = "打撃分析ボタン", modifier = Modifier.size(48.dp))
+                    }
+                    IconButton(onClick = { /*TODO*/ }, modifier = Modifier.weight(1f)) {
+                        Icon(imageVector = Icons.Default.Settings, contentDescription = "設定ボタン", modifier = Modifier.size(48.dp))
+                    }
+                }
+            ) 
+        }
+    ) {
     }
 }
